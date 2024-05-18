54ed8df7d2e90eaae3d1d3ba3bde20653d264874
SARUXSARU <lukasz.muras25@gmail.com>
2024-05-18 13:12:52 +0200

    @SARUXSARU mapa

--- Game/core/src/com/gggeralt/game/screens/gameScreen/GameWorld.java
+++ Game/core/src/com/gggeralt/game/screens/gameScreen/GameWorld.java
@@ -2,122 +2,84 @@ package com.gggeralt.game.screens.gameScreen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-import java.awt.event.KeyListener;
+import javax.print.attribute.standard.PagesPerMinute;
 
 
 public class GameWorld {
-    private int lastKeyCode;
-    private AssetManager assetManager;
-    private Map map;
-    private SpriteBatch spriteBatch;
-    private Viewport viewport;
-    private Texture backgroundTexture;
-    private CollisionElement hero;
-    private CollisionElement brick, brickCeil, brickFloor;
-
-    private Array<Texture> bikerIdle;
-    private float gravity;
+    private final SpriteBatch spriteBatch;
+    private final Viewport viewport;
+   // private final Texture backgroundTexture;
+    private final CollisionElement hero;
+    private final Array<Texture> bikerIdle;
+    private final float gravity;
     private float jumpVelocity;
     private boolean isJumping, isMovingRight, isMovingLeft, isMovingUp;
-    private Array<CollisionElement> bricks;
+    private final Map map;
+    private OrthographicCamera cam;
+
 
 
     public GameWorld() {
-        bricks = new Array<>();
-        this.assetManager = assetManager;
+        cam=new OrthographicCamera();
+        cam.update();
+        map = new Map();
+
         this.spriteBatch = new SpriteBatch();
         bikerIdle = new Array<>();
         bikerIdle.addAll(new Texture(Gdx.files.internal("bikerIdle1.png")), new Texture(Gdx.files.internal("bikerIdle2.png")),
                 new Texture(Gdx.files.internal("bikerIdle3.png")), new Texture(Gdx.files.internal("bikerIdle4.png")));
-
         this.hero = new CollisionElement(bikerIdle, 0, 64);
-        this.brick = new CollisionElement(new Texture(Gdx.files.internal("bricks.png")), 64, 64);
-        this.brickCeil = new CollisionElement(new Texture(Gdx.files.internal("bricks.png")), 0, 256);
-        this.brickFloor = new CollisionElement(new Texture(Gdx.files.internal("bricks.png")), 0, 0);
-        bricks.addAll(brick, brickCeil, brickFloor);
-        this.viewport = new ExtendViewport(1024, 768);
-        this.backgroundTexture = new Texture(Gdx.files.internal("background.png"));
-        gravity = 0.8f;
-        jumpVelocity = 10;
+        this.viewport = new ExtendViewport(1024, 768,cam);
+       // this.backgroundTexture = new Texture(Gdx.files.internal("background.png"));
+        gravity = 0.75f;
+        jumpVelocity = 70;
         isJumping = false;
     }
 
-
     public void update(float delta) {
-
+        handleInput();
+        cameraUpdate();
     }
 
     public void draw() {
-        handleInput();
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        spriteBatch.setProjectionMatrix(cam.combined);
         spriteBatch.begin();
-        spriteBatch.draw(backgroundTexture, 0, 0, 1024, 768);
-        spriteBatch.draw(new Texture("bricks.png"), 0, 0);
-        hero.drawAnimated();
         if (!isJumping && hero.getY() > 64) {
             hero.setY(hero.getY() - gravity);
         }
-        brick.draw();
-        brickFloor.draw();
-        brickCeil.draw();
+        for (CollisionElement brick : map.getBricks()) {
+            brick.draw();
+        }
+        hero.drawAnimated();
         spriteBatch.end();
     }
 
     private void handleInput() {
-        if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) Gdx.app.exit();
-
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) Gdx.app.exit();
         isMovingLeft = Gdx.input.isKeyPressed(Input.Keys.LEFT);
         isMovingRight = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
         isMovingUp = Gdx.input.isKeyJustPressed(Input.Keys.UP);
 
-        if (hero.checkCollision(brick)) {
-            switch (lastKeyCode) {
-                case Input.Keys.LEFT:
-                    hero.moveBy(+200 * Gdx.graphics.getDeltaTime(), 0);
-                    break;
-                case Input.Keys.RIGHT:
-                    hero.moveBy(-200 * Gdx.graphics.getDeltaTime(), 0);
-                    break;
-            }
-        } else {
-            if (isMovingLeft) {
-                lastKeyCode = Input.Keys.LEFT;
-                hero.moveBy(-200 * Gdx.graphics.getDeltaTime(), 0);
-            }
-            if (isMovingRight) {
-                lastKeyCode = Input.Keys.RIGHT;
-                hero.moveBy(+200 * Gdx.graphics.getDeltaTime(), 0);
-            }
-            if (isMovingUp && !isJumping) {
-                lastKeyCode = Input.Keys.UP;
-                isJumping = true;
-                jumpVelocity = 10; // Initial jump velocity
-            }
-            if (isJumping) {
-                hero.setY(hero.getY() + jumpVelocity);
-                jumpVelocity -= gravity;
-                if (jumpVelocity <= 0) {
-                    isJumping = false;
-                }
-            }
+        if (isMovingLeft&&hero.getX()>0) {
+            hero.moveBy(-200 * Gdx.graphics.getDeltaTime(), 0);
         }
-
-    }
-
-
-    public Viewport getViewport() {
-        return viewport;
-    }
-
-    public void jump() {
-        float gravity = 0.8f;
-        float velocity = 10;
+        if (isMovingRight&&hero.getX()<6400) {
+            hero.moveBy(200 * Gdx.graphics.getDeltaTime(), 0);
+        }
+        if (isMovingUp && !isJumping) {
+            isJumping = true;
+            jumpVelocity = 10; // Initial jump velocity
+        }
         if (isJumping) {
             hero.setY(hero.getY() + jumpVelocity);
             jumpVelocity -= gravity;
@@ -125,6 +87,19 @@ public class GameWorld {
                 isJumping = false;
             }
         }
+    }
+
+
+    public Viewport getViewport() {
+        return viewport;
+    }
+    public void cameraUpdate(){
+
+        cam.position.set(10000,10000,0);
+        cam.update();
+        System.out.println(cam.position.x);
+
+
     }
 
 
--- Game/core/src/com/gggeralt/game/screens/gameScreen/Map.java
+++ Game/core/src/com/gggeralt/game/screens/gameScreen/Map.java
@@ -1,16 +1,49 @@
 package com.gggeralt.game.screens.gameScreen;
 
 
-import java.awt.*;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class Map {
+    private final ArrayList<Vector2> coords;
+    private final ArrayList<CollisionElement> bricks;
+    private final float mapWidth;
+    private final float mapHeight;
 
-private Rectangle rectangle;
+    public Map() {
+        coords = new ArrayList<>();
+        bricks = new ArrayList<>();
 
-private Rectangle [][] rectangles;
+        IntStream.range(0, 100).forEach(i -> coords.add(new Vector2(i * 64, 0)));
+        IntStream.range(5, 25).forEach(i -> coords.add(new Vector2(i * 64, 128)));
+        IntStream.range(25, 29).forEach(i -> coords.add(new Vector2(i * 64, 192)));
+        IntStream.range(35, 45).forEach(i -> coords.add(new Vector2(i * 64, 128)));
+        IntStream.range(39, 41).forEach(i -> coords.add(new Vector2(i * 64, 192)));
+        IntStream.range(50, 52).forEach(i -> coords.add(new Vector2(i * 64, 128)));
+        IntStream.range(53, 57).forEach(i -> coords.add(new Vector2(i * 64, 192)));
+        IntStream.range(56, 67).forEach(i -> coords.add(new Vector2(i * 64, 256)));
+        IntStream.range(75, 80).forEach(i -> coords.add(new Vector2(i * 64, 128)));
+        IntStream.range(83, 93).forEach(i -> coords.add(new Vector2(i * 64, 192)));
+        IntStream.range(95, 99).forEach(i -> coords.add(new Vector2(i * 64, 320)));
 
+        coords.forEach(coord -> bricks.add(new CollisionElement(new Texture(Gdx.files.internal("bricks.png")), coord.x, coord.y)));
 
+        mapWidth = 100 * 64;
+        mapHeight = 320 + 64;
+    }
 
+    public ArrayList<CollisionElement> getBricks() {
+        return bricks;
+    }
+    public float getMapWidth() {
+        return mapWidth;
+    }
 
+    public float getMapHeight() {
+        return mapHeight;
+    }
 }
 
